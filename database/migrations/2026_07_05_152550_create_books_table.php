d3076b0adb7090d090002d2bd5bc2f242458a895
ka3gio <wKqahhfn@gmail.com>
2026-07-06 16:46:46 +0900

    feat: マイグレーション作成 #1

---
+++ database/migrations/2026_07_05_152550_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('title', 255);
+            $table->string('author', 100);
+            $table->string('isbn', 13);
+            $table->date('published_date');
+            $table->string('description', 1000);
+            $table->string('image_url', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
